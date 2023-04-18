453ff85149b618317c6a7426fcd486ca794dc12e
Luis Fernando <sloopy.max@gmail.com>
2023-04-17 20:35:23 -0600

    Tabla de videojuegos agregada

--- db.sql
+++ db.sql
@@ -6,3 +6,19 @@ CREATE TABLE Users(
   PRIMARY KEY(id),
   UNIQUE(user)
 );
+
+
+CREATE TABLE Videojuegos(
+  id INT NOT NULL AUTO_INCREMENT,
+  titulo VARCHAR(100) NOT NULL,
+  anio int(4) NOT NULL,
+  director VARCHAR(100) NOT NULL,
+  clasificacion VARCHAR(50) NOT NULL,
+  distribucion VARCHAR(50) NOT NULL,
+  pais VARCHAR(40) NOT NULL,
+  idioma VARCHAR(100) NOT NULL,
+  plataformas VARCHAR(80) NOT NULL,
+  formato VARCHAR(100) NOT NULL,
+  almacenamiento VARCHAR(20) NOT NULL,
+  PRIMARY KEY(id)
+);
